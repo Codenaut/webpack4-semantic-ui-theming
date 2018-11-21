18824dfcc710a2d674f05432c358a8509592e2c5
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2018-11-21 13:48:41 +0100

    webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,68 @@
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.[hash].js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '../../theme.config$': path.join(__dirname, 'my-semantic-theme/theme.config')
-    }
-  },
-  module: {
-    rules: [
-      // this handles .less translation
-      {
-        use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'less-loader']
-        }),
-        test: /\.less$/
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.[hash].js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '../../theme.config$': path.join(__dirname, 'my-semantic-theme/theme.config')
+        }
+    },
+    module: {
+        //CSS, SASS
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            //LESS
+            {
+                test: /\.(less)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "less-loader"
+                ]
+            },
 
-      // this handles images
-      {
-          test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
-          use: 'file-loader?name=[name].[ext]?[hash]'
-      },
+            // this handles images
+            {
+                test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+                use: 'file-loader?name=[name].[ext]?[hash]'
+            },
 
-      // the following rules handle font extraction
-      {
-          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-          test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'file-loader'
-      },
-      {
-          test: /\.otf(\?.*)?$/,
-          use: 'file-loader?name=/fonts/[name].[ext]&mimetype=application/font-otf'
-      }
+            // the following rules handle font extraction
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.otf(\?.*)?$/,
+                use: 'file-loader?name=/fonts/[name].[ext]&mimetype=application/font-otf'
+            }
+        ]
+    },
+    plugins: [
+        // this handles the bundled .css output file
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+            //filename: "./css/[name].css"
+        })
     ]
-  },
-  plugins: [
-    // this handles the bundled .css output file
-    new ExtractTextPlugin({
-      filename: '[name].[contenthash].css'
-    })
-  ]
 };
